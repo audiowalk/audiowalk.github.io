3def28204d8d9b5178c7de36c1ad852fa0b2b989
s9921316 <audiowalk.art@gmail.com>
2021-09-01 15:40:18 +0800

    s

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/audiowalk.github' // test20200915 為 repo 名稱
+      ? '/audiowalk.github.io' // test20200915 為 repo 名稱
       : '/'
   }
